8ce7ff6d50d9a89510b9a1b3a9582caadc832d01
yanzhang <728308386@qq.com>
2023-12-06 15:18:17 +0800

    更新为更合理的平均切割文件

--- split_clipboard.py
+++ split_clipboard.py
@@ -1,35 +1,48 @@
-# clipboard_split.py
-
 import pyperclip
 import os
 
+def find_nearest_sentence_end(text, start, end):
+    # 在指定范围内查找最近的句号位置
+    dot_positions = [text.rfind('.\n', start, end), text.rfind('。\n', start, end)]
+    # 过滤掉找不到的情况
+    valid_positions = [pos for pos in dot_positions if pos != -1]
+
+    if valid_positions:
+        # 返回最近的一个句号位置
+        return max(valid_positions)
+    else:
+        # 如果没有句号，返回原分割点
+        return end
+
 def split_text(text, n):
-    # 分割文本为n份
     segments = []
     segment_length = len(text) // n
     start = 0
+
     for _ in range(n - 1):
-        # 首先尝试找到".\n"
-        end = text.find(".\n", start + segment_length)
-        if end == -1:  # 如果找不到".\n"，则尝试找到“。\n”
-            end = text.find("。\n", start + segment_length)
-            if end == -1:  # 如果仍然找不到，则直接到文本末尾
-                end = len(text)
-        segments.append(text[start:end + 2])  # 包含分割符
-        start = end + 2
-    segments.append(text[start:])  # 添加最后一段
+        end = start + segment_length
+        if end >= len(text):
+            end = len(text) - 1
+
+        nearest_end = find_nearest_sentence_end(text, start, end)
+        segments.append(text[start:nearest_end + 1])
+        start = nearest_end + 1
+
+    if start < len(text):
+        segments.append(text[start:])
+
     return segments
 
 def save_segments(n, save_path="/Users/yanzhang/Downloads"):
-    # 从剪贴板读取文本
     text = pyperclip.paste()
-
-    # 分割文本
     segments = split_text(text, n)
 
-    # 直接保存到指定路径
     for i, segment in enumerate(segments):
-        file_path = os.path.join(save_path, f'segment_{i + 1}.txt')
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(segment)
-        print(f"第{i + 1}部分已保存到: {file_path}")
+        if segment:
+            file_path = os.path.join(save_path, f'segment_{i + 1}.txt')
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(segment)
+            print(f"第{i + 1}部分已保存到: {file_path}")
+
+# 使用示例
+# save_segments(5)

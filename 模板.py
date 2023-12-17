c4d97d0061093d8c28201b81648867d92f31f1cc
yanzhang <728308386@qq.com>
2023-12-17 09:30:22 +0800

    完成经济学人网站的抓取比较存储等功能

--- 模板.py
+++ 模板.py
@@ -1,3 +1,13 @@
+# 各种前置导入
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import datetime
+from time import sleep
+
 # 点击economist的log in按钮
 try:
     # 定位登录链接并点击
@@ -7,4 +17,5 @@ try:
 except Exception as e:
     print("登录异常:", e)
 
-#
+# 等待100秒
+sleep(110)

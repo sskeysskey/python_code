c196601d7770cdc583da08a96f99a2cdfbeb2921
yanzhang <728308386@qq.com>
2024-01-05 11:28:03 +0800

    更新虚拟环境文件

--- check_venv.py
+++ check_venv.py
@@ -2,11 +2,12 @@ import os
 import subprocess
 
 def check_virtualenv(path):
+    activate_script = os.path.join(path, 'bin', 'activate')
     if os.path.exists(path) and os.path.isdir(path):
-        if os.path.exists(os.path.join(path, 'bin', 'activate')):
+        if os.path.exists(activate_script):
             print(f"{path} 是一个Python虚拟环境。")
-            subprocess.run(["source", os.path.join(path, "bin", "activate")], shell=True)
-            subprocess.run(["pip", "list"])
+            # 在一个新的shell里面运行source命令激活虚拟环境
+            subprocess.run(f"source {activate_script} && pip list", shell=True, executable='/bin/bash')
         else:
             print(f"{path} 不是一个Python虚拟环境。")
     else:

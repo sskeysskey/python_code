29aff5ad310b0e97d48fb7d41278b9ee26a43d8e
yanzhang <728308386@qq.com>
2024-01-11 16:00:07 +0800

    实现了比较一张截图和整个屏幕上完全一致的像素位置，程序终于可以看见东西了。

--- check_write_permission.py
+++
@@ -1,7 +0,0 @@
-# check_write_permission.py
-try:
-    with open('/Users/yanzhang/Documents/test_permission.txt', 'w') as f:
-        f.write('Test')
-    print("Write permission confirmed.")
-except PermissionError:
-    print("No write permission.")

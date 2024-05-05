8681935514f205a950b81fdefeb6678d40ee76f2
sskeysskey <728308386@qq.com>
2024-05-05 21:33:59 +0800

    daily update

--- test.py
+++ test.py
@@ -1,38 +1,98 @@
-from bokeh.plotting import figure, output_file, save, show
-from bokeh.models import ColumnDataSource, DatetimeTickFormatter
-import sqlite3
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
+import sqlite3
 
-def fetch_data(name):
+def get_parent_id(commodity):
+    if commodity in ["Crude Oil", "Brent", "Natural gas", "Coal", "Uranium"]:
+        return 5
+    elif commodity in ["Gold", "Silver", "Copper", "Steel", "Lithium"]:
+        return 6
+    elif commodity in ["Soybeans", "Wheat", "Palm Oil", "Orange Juice", "Cocoa", "Rice", "Corn", "Coffee", "Sugar",
+        "Oat", "Cotton"]:
+        return 7
+    elif commodity in ["Aluminum", "Nickel", "Tin", "Zinc", "Palladium"]:
+        return 8
+    elif commodity in ["Live Cattle", "Lean Hogs", "Poultry", "Salmon"]:
+        return 9
+    elif commodity in ["CRB Index", "LME Index", "Nuclear Energy Index", "Solar Energy Index", "EU Carbon Permits",
+        "Containerized Freight Index"]:
+        return 23
+    return None
+
+# 获取当前时间
+now = datetime.now()
+
+# 判断今天的星期数，如果是周日(6)或周一(0)，则不执行程序
+if now.weekday() in (0, 6):
+    print("Today is either Sunday or Monday. The script will not run.")
+else:
+    # 初始化数据库连接
     conn = sqlite3.connect('/Users/yanzhang/Documents/Database/Finance.db')
     cursor = conn.cursor()
-    three_months_ago = datetime.now() - timedelta(days=90)
-    query = "SELECT date, price FROM Stocks WHERE name = ? AND date >= ? ORDER BY date;"
-    cursor.execute(query, (name, three_months_ago.strftime("%Y-%m-%d")))
-    data = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return data
+    # 创建表
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Stocks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT,
+        name TEXT,
+        price REAL,
+        parent_id INTEGER,
+        FOREIGN KEY (parent_id) REFERENCES Categories(id)
+    );
+    ''')
+    conn.commit()
 
-def create_html_plot(name):
-    data = fetch_data(name)
-    if data:
-        dates, prices = zip(*data)
-        dates = [datetime.strptime(d, "%Y-%m-%d").date() for d in dates]
-    else:
-        dates, prices = [], []
+    # ChromeDriver 路径
+    chrome_driver_path = "/Users/yanzhang/Downloads/backup/chromedriver"
+    service = Service(executable_path=chrome_driver_path)
 
-    source = ColumnDataSource(data=dict(date=dates, price=prices))
+    # 设置WebDriver
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')  # 无界面模式
+    driver = webdriver.Chrome(service=service, options=options)
 
-    plot = figure(title=f"{name} - Price Chart", x_axis_type="datetime", sizing_mode="stretch_width", height=250)
-    plot.line('date', 'price', source=source, line_width=2)
-    
-    plot.xaxis.formatter = DatetimeTickFormatter(months="%b %Y")
-    plot.xaxis.major_label_orientation = 3.14/4
+    try:
+        # 访问网页
+        driver.get('https://finance.yahoo.com/commodities/')
+        # 映射表将商品符号与商品名称关联
+        commodity_mapping = {
+            "CC=F": "Cocoa", "KC=F": "Coffee", "CT=F": "Cotton", "OJ=F": "Orange Juice", "SB=F": "Sugar",
+            "HE=F": "Lean Hogs", "CL=F": "Crude Oil", "BZ=F": "Brent", "LE=F": "Live Cattle", "HG=F": "Copper",
+            "ZC=F": "Corn", "GC=F": "Gold", "SI=F": "Silver", "NG=F": "Natural gas", "ZO=F": "Oat",
+            "ZR=F": "Rice", "ZS=F": "Soybeans"
+        }
 
-    output_file(f"{name}.html")
-    save(plot)
-    show(plot)
+        all_data = []
+        # 获取当前时间
+        now = datetime.now()
+        # 获取前一天的日期
+        yesterday = now - timedelta(days=1)
+        # 格式化输出
+        today = yesterday.strftime('%Y-%m-%d')
 
-# 生成具体某个产品的图表HTML
-create_html_plot('NASDAQ')
+        symbols = driver.find_elements(By.XPATH, '//a[@data-test="quoteLink"]')
+        for symbol in symbols:
+            commodity_symbol = symbol.text
+            if commodity_symbol in commodity_mapping:
+                commodity_name = commodity_mapping[commodity_symbol]
+                parent_id = get_parent_id(commodity_name)
+                price_element = symbol.find_element(By.XPATH, './ancestor::td/following-sibling::td[@aria-label="Last Price"]/fin-streamer')
+                price = price_element.get_attribute('value')
+                all_data.append((today, commodity_name, price, parent_id))
+        
+        # 插入数据到数据库
+        cursor.executemany('INSERT INTO Stocks (date, name, price, parent_id) VALUES (?, ?, ?, ?)', all_data)
+        conn.commit()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        conn.rollback()  # 回滚在异常发生时的所有操作
+    finally:
+        driver.quit()
+        conn.close()
+
+    print("Data scraping and storage done.")
